4826273d30caf03f2a4c3428d24c91aea66d1bd8
Koichi Taura <taura.koichi@gmail.com>
2020-04-05 09:38:28 +0900

    move equals function to parent to make it common

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -5,8 +5,4 @@ public class Dollar extends Money {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
-    }
 }
