bd193367f0ea72abdfc72f971df73eea9e8d48bb
Koichi Taura <taura.koichi@gmail.com>
2020-04-05 15:41:35 +0900

    modify comparison logic using class

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -2,6 +2,6 @@ public class Money {
     protected int amount;
     public boolean equals(Object object) {
         Money dollar = (Money) object;
-        return amount == dollar.amount;
+        return amount == dollar.amount && getClass().equals(object.getClass());
     }
 }
